363bc05f675c56dd2aefca472020a7a752e15c52
Paco Segovia <pacosegovia@gmail.com>
2019-10-31 15:53:43 +0100

    taskbox UI

---
+++ storybook-static/main.2b279163f64ff125f2d3.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(415),c(566),n.exports=c(850)},1:function(n,o){},566:function(n,o,c){"use strict";c.r(o);c(567),c(847)}},[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.878f5549d7d564f6028586b4753e2e38.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff31e58f5c0c6ca14c7e39b31fb2192",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5ea91f49acc0ab7cdd18.bundle.js"
+  },
+  {
+    "revision": "c0246701a2b3df27b423",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.5ea91f49acc0ab7cdd18.bundle.js"
+  },
+  {
+    "url": "vendors~main.5ea91f49acc0ab7cdd18.bundle.js"
+  }
+]);
